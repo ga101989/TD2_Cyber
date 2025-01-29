e178d1ce1b22b1165ea80cbb5c8cd43c2344c322
ga101989 <113102996+ga101989@users.noreply.github.com>
2025-01-29 14:32:11 +0100

    Update of test.php
    Update 1High

--- ex_2/1/test.php
+++ ex_2/1/test.php
@@ -9,7 +9,8 @@ libxml_disable_entity_loader (false);
 $xml = strlen($_GET['xml']) > 0 ? $_GET['xml'] : '<root><content>No XML found</content></root>';
 
 $document = new DOMDocument();
-$document->loadXML($xml, LIBXML_NOENT | LIBXML_DTDLOAD);
+//$document->loadXML($xml, LIBXML_NOENT | LIBXML_DTDLOAD);
+$document->loadXML($xml, LIBXML_NONET | LIBXML_NOERROR | LIBXML_NOWARNING);
 $parsedDocument = simplexml_import_dom($document);
 
 echo $parsedDocument->content;
